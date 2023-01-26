fcac415a9b560ca3ef0027b8bde37b706da8a03d
mdkwe <michelle.dkwe@gmail.com>
2023-01-26 20:46:11 +0100

    Return hash with currency names & values

--- lib/app.rb
+++ lib/app.rb
@@ -11,16 +11,28 @@ def open_page(url_html)
   return Nokogiri::HTML(URI.open(url_html))
 end
 
+def fusiontab(tab1,tab2)
+  return Hash[tab1.zip(tab2)]
+end
+
 def scrapping_currencies(page)
   # Search for nodes by xpath
   
   currencies_name = []
+  currencies_value = []
+  hash={}
 
-  page.xpath("//*[contains(@class,'cmc-table__column-name--name cmc-link')]").each do |currency|
-    currencies_name.append(currency.content)
+  page.xpath("//*[contains(@class,'cmc-table__column-name--name cmc-link')]").each do |name|
+    currencies_name.append(name.content)
   end
 
-  return currencies_name
+  page.xpath('//tr//td[5]/div/a/span').each do |value|
+    currencies_value.append(value.content)
+  end
+
+  hash=fusiontab(currencies_name,currencies_value)
+  return hash
+
 end 
 
 def perform()
@@ -29,7 +41,6 @@ def perform()
   currency_array = scrapping_currencies(page)
   puts "Number of element : #{currency_array.size()}"
   print currency_array
-
 end
 
 perform
